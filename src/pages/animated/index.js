2d482a9e0ea04f9240688340e7d2fd06c670d1b5
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-07-04 10:45:33 +0300

    fix: fixed animated

--- src/pages/animated/index.js
+++ src/pages/animated/index.js
@@ -1,10 +1,22 @@
 import React,  {Component} from 'react'
 import './styles.css'
 
-const AnimatedPage = (props) => {
-  return (
-    props.start ? <div className="loader loader-animated"/> : <div>Load</div>
-  )
+class AnimatedPage extends React.Component{
+    state = {
+        anime: false
+    }
+
+    changeState = () => {
+        this.state.anime ? this.setState({anime: false}) : this.setState({anime: true})
+    }
+
+    render() {
+        return (
+            <div className="page__anime"></div>
+        )
+    }
+        
+    
 }
   
 export default AnimatedPage
