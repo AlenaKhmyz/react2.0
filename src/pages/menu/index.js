62d590aa55b6542307238b5eb27be57ab14a5465
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-07-05 04:10:44 +0300

    fix: fixed menu

--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,27 +1,61 @@
 import React,  {Component} from 'react'
 import './styles.css'
 
-class MenuPage extends React.Component{
-    ingredients = [
-        {text: 'pepperoni', id:'pizza'},
-        {text:'pineapple', id:'fruit'},
-        {text: 'cheese', id: 'cheese'}, 
-        {text:'mushrooms', id: 'mushroom'}
-    ]
+const ingredients = ['pepperoni','pineapple', 'cheese', 'mushrooms']
 
+
+class CountIngridients extends React.Component {
     state = {
-        list: false
+        count: 0
     }
 
-    changeState = () => {
-       this.setState({list: !this.state.list})
+    onAddCount = () => {
+        this.setState({count: this.state.count + 1})
+        this.props.sum(1) 
     }
+    onDeleteCount = () => {
+        if(this.state.count > 0) {
+            this.setState({count: this.state.count - 1}) 
+            this.props.sum(-1)
+        }
+        else
+            this.setState({count: 0})
+    }
+
+    render () {
+        return (<span>
+            <button onClick={this.onDeleteCount}>-</button>
+            <span>{this.state.count}</span>
+            <button onClick ={this.onAddCount}>+</button>
+        </span>)
+    }
+}
+
+
+class MenuPage extends React.Component{
+    
+    state = {
+        list: false,
+        endingCount: 0
+    }
+
+    onShowItem = () => this.setState({list: !this.state.list})
+
+    plusCount = (number) =>  this.setState({endingCount: this.state.endingCount + number})
 
     render() {
         return (
-            <div className="page__menu">
-              
-            </div>
+            this.state.list ? (
+                <div className ={this.props.direction }> 
+                <button onClick = {this.onShowItem} style = {{width: '300px'}}>Close▲</button>
+                    <ul>
+                        {ingredients.map( item => <li><span>{item} <CountIngridients sum={this.plusCount} /></span></li>)}
+                    </ul>
+                    <div>{this.state.finalCount}</div>
+                </div>
+               )
+               :
+               <button onClick = {this.onShowItem} style = {{width: '300px'}}>Ingredients▼</button>
         )
     }
         
