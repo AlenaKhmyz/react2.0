bf711c15d49b1d556d17d09aa5af6cf58a36faab
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-07-05 03:50:39 +0300

    fix: fixed drop down

--- src/pages/dropDown/index.js
+++ src/pages/dropDown/index.js
@@ -2,18 +2,32 @@ import React,  {Component} from 'react'
 import './styles.css'
 
 class DropDownPage extends React.Component{
+    ingredients = [
+        {text: 'pepperoni', id:'pizza'},
+        {text:'pineapple', id:'fruit'},
+        {text: 'cheese', id: 'cheese'}, 
+        {text:'mushrooms', id: 'mushroom'}
+    ]
+
     state = {
-        anime: false
+        list: false
     }
 
     changeState = () => {
-       
+       this.setState({list: !this.state.list})
     }
 
     render() {
         return (
             <div className="page__drop-down">
-               
+               <button onClick={this.changeState} className="drop-down__btn">
+                    Ingredients
+                    {this.state.list ? '▲' : '▼'}
+               </button>
+
+               {this.state.list && this.ingredients.map((item) => {
+                return <div key={item.id}>{item.text}</div>
+            })}
             </div>
         )
     }
