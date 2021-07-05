8cff147999f4e49cbf62e272fbbfb3bad8952942
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-07-06 01:27:28 +0300

    fix: fixed modification

--- src/pages/modification/index.js
+++ src/pages/modification/index.js
@@ -1,19 +1,34 @@
 import React,  {Component} from 'react'
 import './styles.css'
 
+class Button extends React.Component{
+    render() {
+        return (
+            <button onClick={this.props.click}>{this.props.label}</button>
+        )
+    }
+}
+
+
 class ModificationPage extends React.Component{
     state = {
         num: 0
     }
 
-    changeState = () => {
-        
+    changePlus = () => {
+        this.setState({num: this.state.num + 1})
+    }
+
+    changeMinus = () => {
+        this.setState(this.state.num > 0 ? {num: this.state.num - 1} : {num: 0})
     }
 
     render() {
         return (
             <div className="page__modification">
-                
+                <Button label="-" click={this.changeMinus}></Button>
+                {this.state.num}
+                <Button label="+" click={this.changePlus}></Button>
             </div>
         )
     }
